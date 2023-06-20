33ae1292b7ec1e91cdc6f258ae2675167b069174
X13-A <foulonalex@yahoo.com>
2023-06-20 19:34:58 +0200

    Update model to set columns to null when foreign key deleted

--- SQL/create/CreateAll.sql
+++ SQL/create/CreateAll.sql
@@ -24,12 +24,12 @@ CREATE TABLE Pizza_Order(
    order_id INT AUTO_INCREMENT,
    order_status VARCHAR(50) NOT NULL,
    order_datetime DATETIME NOT NULL,
-   pizza_id INT NOT NULL,
+   customer_id INT,
+   pizza_id INT,
    pizza_size VARCHAR(50) DEFAULT 'medium',
-   customer_id INT NOT NULL,
    PRIMARY KEY(order_id),
-   FOREIGN KEY(pizza_id) REFERENCES Pizza(pizza_id),
-   FOREIGN KEY(customer_id) REFERENCES Customer(customer_id)
+   FOREIGN KEY(pizza_id) REFERENCES Pizza(pizza_id) ON DELETE SET NULL,
+   FOREIGN KEY(customer_id) REFERENCES Customer(customer_id) ON DELETE SET NULL
 );
 
 CREATE TABLE Vehicle(
@@ -46,31 +46,37 @@ CREATE TABLE Delivery_Person(
 );
 
 CREATE TABLE Invoice(
-   invoice_id INT AUTO_INCREMENT,
-   order_id INT NOT NULL,
-   PRIMARY KEY(invoice_id),
-   UNIQUE(order_id),
-   FOREIGN KEY(order_id) REFERENCES Pizza_Order(order_id)
+   invoice_id INT,
+   pizza_id INT,
+   pizza_name VARCHAR(50),
+   pizza_size VARCHAR(50),
+   total_price FLOAT,
+   customer_id INT,
+   customer_name VARCHAR(50),
+   order_time DATETIME,
+   delivery_time DATETIME,
+   PRIMARY KEY(invoice_id)
 );
+
 
 CREATE TABLE Delivery(
    delivery_id INT AUTO_INCREMENT,
    delivery_status VARCHAR(24),
    delivery_datetime DATETIME,
-   delivery_person_id INT NOT NULL,
-   vehicle_id INT NOT NULL,
-   order_id INT NOT NULL,
+   delivery_person_id INT,
+   vehicle_id INT,
+   order_id INT,
    PRIMARY KEY(delivery_id),
    UNIQUE(order_id),
-   FOREIGN KEY(delivery_person_id) REFERENCES Delivery_Person(delivery_person_id),
-   FOREIGN KEY(vehicle_id) REFERENCES Vehicle(vehicle_id),
-   FOREIGN KEY(order_id) REFERENCES Pizza_Order(order_id)
+   FOREIGN KEY(delivery_person_id) REFERENCES Delivery_Person(delivery_person_id) ON DELETE SET NULL,
+   FOREIGN KEY(vehicle_id) REFERENCES Vehicle(vehicle_id) ON DELETE SET NULL,
+   FOREIGN KEY(order_id) REFERENCES Pizza_Order(order_id) ON DELETE SET NULL
 );
 
 CREATE TABLE Has_Ingredient(
-   pizza_id INT,
-   ingredient_id INT,
+   pizza_id INT NOT NULL,
+   ingredient_id INT NOT NULL,
    PRIMARY KEY(pizza_id, ingredient_id),
-   FOREIGN KEY(pizza_id) REFERENCES Pizza(pizza_id),
-   FOREIGN KEY(ingredient_id) REFERENCES Ingredient(ingredient_id)
+   FOREIGN KEY(pizza_id) REFERENCES Pizza(pizza_id) ON DELETE CASCADE,
+   FOREIGN KEY(ingredient_id) REFERENCES Ingredient(ingredient_id) ON DELETE CASCADE
 );
--- SQL/requests/OrdersDetails.sql
+++ SQL/requests/OrdersDetails.sql
@@ -2,7 +2,7 @@ DELIMITER //
 CREATE PROCEDURE GetOrdersDetails()
 BEGIN
     SELECT Customer.customer_name, Pizza.pizza_name,
-           CalculateAdjustedPrice(Pizza.pizza_id) AS adjusted_price,
+           CalculateAdjustedPrice(Pizza.pizza_id, Pizza_Order.pizza_size) AS adjusted_price,
            Pizza_Order.order_status, Pizza_Order.order_datetime
     FROM Pizza_Order
     JOIN Customer ON Pizza_Order.customer_id = Customer.customer_id
