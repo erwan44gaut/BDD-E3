40eed8648ff015f8fea69488543bfc4e938968e9
lemagnyt <lemagnythomas@gmail.com>
2023-06-21 00:08:06 +0200

    stats changed

--- SQL/stats/BestCustomer.sql
+++ SQL/stats/BestCustomer.sql
@@ -1,9 +1,16 @@
 DELIMITER //
 CREATE PROCEDURE GetBestCustomers()
 BEGIN
-    SELECT customer_id, COUNT(*) AS total_deliveries
-    FROM Delivery
+    SELECT customer_id, COUNT(*) AS total_sales
+    FROM Pizza_Order AS o
+    JOIN Delivery ON o.order_id = Delivery.order_id
+    WHERE customer_id IS NOT NULL
     GROUP BY customer_id
-    HAVING COUNT(*) = (SELECT MAX(delivery_count) FROM (SELECT COUNT(*) AS delivery_count FROM Delivery GROUP BY customer_id) AS temp);
-END;
-DELIMITER;
+    HAVING COUNT(*) = (SELECT MAX(sales_count)
+                        FROM (SELECT COUNT(*) AS sales_count 
+                            FROM Pizza_Order AS o
+                            JOIN Delivery ON o.order_id = Delivery.order_id 
+                            WHERE customer_id IS NOT NULL
+                            AND (Delivery.delivery_status="COMPLETE" OR Delivery.delivery_status="LATE")
+                            GROUP BY customer_id) AS temp);
+END//
--- SQL/stats/BestIngredient.sql
+++ SQL/stats/BestIngredient.sql
@@ -1,11 +1,18 @@
 DELIMITER //
 CREATE PROCEDURE GetBestIngredients()
 BEGIN
-    SELECT ingredient_id, COUNT(*) AS total_sales
+SELECT ingredient_id, COUNT(*) AS total_sales
     FROM Has_Ingredient AS h
     JOIN Pizza_Order ON h.pizza_id = Pizza_Order.pizza_id
     JOIN Delivery ON Pizza_Order.order_id = Delivery.order_id
+    WHERE h.pizza_id IS NOT NULL AND ingredient_id IS NOT NULL
     GROUP BY ingredient_id
-    HAVING COUNT(*) = (SELECT MAX(sales_count) FROM (SELECT COUNT(*) AS sales_count FROM Has_Ingredient JOIN Pizza_Order ON Has_Ingredient.pizza_id = Pizza_Order.pizza_id JOIN Delivery ON Pizza_Order.order_id = Delivery.order_id GROUP BY ingredient_id) AS temp);
-END
-DELIMITER;
+    HAVING COUNT(*) = (SELECT MAX(sales_count) 
+                        FROM (SELECT COUNT(*) AS sales_count 
+                                FROM Has_Ingredient
+                                JOIN Pizza_Order ON Has_Ingredient.pizza_id = Pizza_Order.pizza_id 
+                                JOIN Delivery ON Pizza_Order.order_id = Delivery.order_id 
+                                WHERE Has_Ingredient.pizza_id IS NOT NULL AND ingredient_id IS NOT NULL
+                                AND (Delivery.delivery_status="COMPLETE" OR Delivery.delivery_status="LATE")
+                                GROUP BY ingredient_id) AS temp);
+END//
