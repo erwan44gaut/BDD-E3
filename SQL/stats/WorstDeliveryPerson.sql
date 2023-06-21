c603777067c78569cdd4df2289784fa22169f850
lemagnyt <lemagnythomas@gmail.com>
2023-06-21 09:22:25 +0200

    correct status and delete ingredient

--- SQL/stats/WorstDeliveryPerson.sql
+++ SQL/stats/WorstDeliveryPerson.sql
@@ -6,7 +6,7 @@ BEGIN
     SELECT MIN(passed_deliveries - failed_deliveries) INTO max_difference
     FROM (
         SELECT delivery_person_id, 
-               SUM(CASE WHEN delivery_status = 'PASSED' THEN 1 ELSE 0 END) AS passed_deliveries,
+               SUM(CASE WHEN delivery_status = 'COMPLETE' THEN 1 ELSE 0 END) AS passed_deliveries,
                SUM(CASE WHEN (delivery_status = 'LATE' OR delivery_status = 'CANCELED') THEN 1 ELSE 0 END) AS failed_orders
         FROM Delivery
         GROUP BY delivery_person_id
@@ -15,7 +15,7 @@ BEGIN
     SELECT delivery_person_id, passed_deliveries - failed_deliveries AS difference
     FROM (
         SELECT delivery_person_id, 
-               SUM(CASE WHEN delivery_status = 'PASSED' THEN 1 ELSE 0 END) AS passed_deliveries,
+               SUM(CASE WHEN delivery_status = 'COMPLETE' THEN 1 ELSE 0 END) AS passed_deliveries,
                SUM(CASE WHEN (delivery_status = 'LATE' OR delivery_status = 'CANCELED') THEN 1 ELSE 0 END) AS failed_orders
         FROM orders
         GROUP BY delivery_person_id
