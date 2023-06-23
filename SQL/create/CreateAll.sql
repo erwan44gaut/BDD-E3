98ecdb13a92c3d722da08a3a8f5b50bc16facafe
X13-A <foulonalex@yahoo.com>
2023-06-23 17:57:16 +0200

    Fix auto increment not working

--- SQL/create/CreateAll.sql
+++ SQL/create/CreateAll.sql
@@ -1,53 +1,47 @@
 use pizzeria;
 
 CREATE TABLE Pizza(
-   pizza_id INT AUTO_INCREMENT,
+   pizza_id INT AUTO_INCREMENT PRIMARY KEY,
    pizza_base_price DECIMAL(10,2) NOT NULL,
    pizza_name VARCHAR(50) NOT NULL,
-   PRIMARY KEY(pizza_id),
    UNIQUE(pizza_name)
 );
 
 CREATE TABLE Ingredient(
-   ingredient_id INT AUTO_INCREMENT,
+   ingredient_id INT AUTO_INCREMENT PRIMARY KEY,
    ingredient_name VARCHAR(50) NOT NULL,
-   PRIMARY KEY(ingredient_id),
    UNIQUE(ingredient_name)
 );
 
 CREATE TABLE Customer(
-   customer_id INT AUTO_INCREMENT,
+   customer_id INT AUTO_INCREMENT PRIMARY KEY,
    customer_name VARCHAR(50) NOT NULL,
-   customer_balance DECIMAL(10,2) DEFAULT(0),
-   PRIMARY KEY(customer_id)
+   customer_balance DECIMAL(10,2) DEFAULT(0)
 );
 
 CREATE TABLE Pizza_Order(
-   order_id INT AUTO_INCREMENT,
+   order_id INT AUTO_INCREMENT PRIMARY KEY,
    order_status ENUM('ACCEPTED', 'IN_PREPARATION', 'IN_DELIVERY', 'COMPLETED', 'CANCELED') DEFAULT 'ACCEPTED',
    order_datetime DATETIME DEFAULT NOW(),
    customer_id INT,
    pizza_id INT,
    pizza_size ENUM('SMALL', 'MEDIUM', 'LARGE') DEFAULT 'MEDIUM',
-   PRIMARY KEY(order_id),
    FOREIGN KEY(pizza_id) REFERENCES Pizza(pizza_id) ON DELETE SET NULL,
    FOREIGN KEY(customer_id) REFERENCES Customer(customer_id) ON DELETE SET NULL
 );
 
 CREATE TABLE Vehicle(
-   vehicle_id INT AUTO_INCREMENT,
-   vehicle_type ENUM('CAR', 'MOTORBIKE') DEFAULT 'CAR',
-   PRIMARY KEY(vehicle_id)
+   vehicle_id INT AUTO_INCREMENT  PRIMARY KEY,
+   vehicle_type ENUM('CAR', 'MOTORBIKE') DEFAULT 'CAR'
 );
 
 CREATE TABLE Delivery_Person(
-   delivery_person_id INT AUTO_INCREMENT,
-   delivery_person_name VARCHAR(50) NOT NULL,
-   PRIMARY KEY(delivery_person_id)
+   delivery_person_id INT AUTO_INCREMENT PRIMARY KEY,
+   delivery_person_name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Invoice(
-   invoice_id INT,
+   invoice_id INT AUTO_INCREMENT PRIMARY KEY,
    pizza_id INT,
    pizza_name VARCHAR(50),
    pizza_size VARCHAR(50),
@@ -55,19 +49,17 @@ CREATE TABLE Invoice(
    customer_id INT,
    customer_name VARCHAR(50),
    order_time DATETIME,
-   delivery_time DATETIME,
-   PRIMARY KEY(invoice_id)
+   delivery_time DATETIME
 );
 
 
 CREATE TABLE Delivery(
-   delivery_id INT AUTO_INCREMENT,
+   delivery_id INT AUTO_INCREMENT PRIMARY KEY,
    delivery_status ENUM('ACCEPTED', 'IN_PROGRESS', 'COMPLETE', 'LATE'),
    delivery_datetime DATETIME,
    delivery_person_id INT,
    vehicle_id INT,
    order_id INT,
-   PRIMARY KEY(delivery_id),
    UNIQUE(order_id),
    FOREIGN KEY(delivery_person_id) REFERENCES Delivery_Person(delivery_person_id) ON DELETE SET NULL,
    FOREIGN KEY(vehicle_id) REFERENCES Vehicle(vehicle_id) ON DELETE SET NULL,
